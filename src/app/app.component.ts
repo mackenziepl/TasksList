f2d3da3fc84350990d0c119886b7e0762dd8e7d8
“ <mackenziepl@poczta.onet.pl>
2019-01-11 19:48:50 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-project';
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+  }
+
+  delete(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+      this.tasksDone.push(task);
+      this.delete(task);
+  }
 }
